88600f32acfd8fdfbabd33dac56817dd06ab2a60
Petre4488 <petrecirceagwork@gmail.com>
2024-03-27 11:23:08 +0200

    homepage-first

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import NavBar from '../Components/NavBar'
+import BackgroundBeamsDemo from '../Components/Background'
+import CanvasRevealEffectDemo from '../Components/CardsHP'
+
+export default function Home() {
+  return (
+    <>
+      <NavBar />
+      <BackgroundBeamsDemo />
+      <CanvasRevealEffectDemo />
+    </>
+  );
+}
