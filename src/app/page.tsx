bc60885ba1e8e91e2979b97174edba6eaf644858
Petre4488 <petrecirceagwork@gmail.com>
2024-03-28 10:09:11 +0200

    css fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import NavBar from '../Components/NavBar'
 import BackgroundBeamsDemo from '../Components/Background'
 import CanvasRevealEffectDemo from '../Components/CardsHP'
+import './globals.css'
 
 export default function Home() {
   return (
