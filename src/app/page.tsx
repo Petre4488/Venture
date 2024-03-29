05e71c50f6a450904718c04e42a24169a5ec68a1
Petre4488 <petrecirceagwork@gmail.com>
2024-03-29 08:43:58 +0200

    fix#4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
 import NavBar from '../Components/NavBar'
 import BackgroundBeamsDemo from '../Components/Background'
 import CanvasRevealEffectDemo from '../Components/CardsHP'
-import styles from './globals.css'
 
 export default function Home() {
   return (
     <>
-      <div className={styles.wrapper}>
-        <NavBar />
-        <BackgroundBeamsDemo />
-        <CanvasRevealEffectDemo />
-      </div>
+      <NavBar />
+      <BackgroundBeamsDemo />
+      <CanvasRevealEffectDemo />
     </>
   );
 }
